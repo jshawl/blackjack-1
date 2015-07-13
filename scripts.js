448851d06ba205f44337c967a6aa7a8c531377a8
Jeff Jones <jeff.jones1@gmail.com>
2015-07-13 14:10:13 -0400

    functioning player hit

--- scripts.js
+++ scripts.js
@@ -10,20 +10,46 @@ for (var i = 0; i< 52; i++){
 
 var cardValue = function (card) {
   var index = Math.floor((card/4) + 1);
-  if (index == 1){index = 11;}
+  if (index === 1){index = 11;}
   if (index > 10){index = 10;}
   return index;
 }
 
 var cardString = function(card){
   var index =  Math.floor((card/4) + 1);
-  var suits = ['&spades;', '&clubs;', '&hearts;', '&diamonds;'];
-  var string =''
-  if (card % 4 === 0){string += suits[0]}
-  $('.card').html(string);
+  var suit = card % 4;
+  var suits = ['&spades;', '&clubs;', '&hearts;', '&diams;'];
+  var string ='';
+    //# assignment based on cards 0-14 A low, K high
+    if (1 < index < 11){string += index}
+    if (index === 13){string = 'K'}
+    if (index === 12){string = 'Q'}
+    if (index === 11){string = 'J'}
+    if (index === 1){string = 'A'}
+    //suit assignment based on remainder
+    if (suit === 0){string += suits[0]}
+    if (suit === 1){string += suits[1]}
+    if (suit === 2){string += suits[2]}
+    if (suit === 3){string += suits[3]}
+    return string;
 }
 
-console.log(deck)
+var toHand = function(card) {
+  var playerCard = deck.pop();
+  return playerCard;
+}
+$(document).ready (function (){
+    var div = -1;
+
     $('#hit').on('click', function (){
-      console.log('test')
-    })
+      var string = cardString(toHand());
+      $('<div><div>').addClass('card').appendTo('.playerArea');
+        div ++
+
+        $('.card').eq(div).html(string);
+  })
+})
+
+var playerTotal = function (card) {
+
+}
